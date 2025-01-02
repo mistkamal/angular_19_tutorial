42af7e9fcaae0e165da6b84e8b099771aea83336
KAMAL <mistkamal@gmail.com>
2025-01-02 18:18:54 +0530

    Api integration Add update delete

--- src/app/components/apiIntegration/post-api/post-api.component.ts
+++ src/app/components/apiIntegration/post-api/post-api.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject,OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,38 +10,68 @@ import { FormsModule } from '@angular/forms';
 })
 export class PostApiComponent implements OnInit {
 
-  departmentlist:any[]=[];
-  depatObj:any={
+  departmentlist: any[] = [];
+  depatObj: any = {
     "departmentId": 0,
     "departmentName": "",
     "departmentLogo": ""
   };
 
   http = inject(HttpClient);
-ngOnInit(): void {
-  this.getdepartment();
-}
-  onSave(){
+  ngOnInit(): void {
+    this.getdepartment();
+  };
+
+  onSave() {
     debugger
-    this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment",this.depatObj).subscribe((res:any)=>{
+    this.http.post("https://projectapi.gerasim.in/api/Complaint/AddNewDepartment", this.depatObj).subscribe((res: any) => {
       debugger
-      if(res.result){
+      if (res.result) {
         alert('department add succesfull');
         this.getdepartment();
       }
-      else{
+      else {
         alert(res.message);
       }
     })
-  }
+  };
 
-  getdepartment(){
-    this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((res:any)=>{
-    this.departmentlist = res.data;
+  getdepartment() {
+    this.http.get("https://projectapi.gerasim.in/api/Complaint/GetParentDepartment").subscribe((res: any) => {
+      this.departmentlist = res.data;
     })
-  }
+  };
 
-  onEdit(data:any){
-    this.depatObj=data;
+  onEdit(data: any) {
+    this.depatObj = data;
+  };
+
+  onUpdate(data: any) {
+    this.depatObj = data;
+    this.http.put("", data).subscribe((res: any) => {
+      if (res.result) {
+        alert('department Update succesfull');
+        this.getdepartment();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  };
+
+  onDelete(myid: number) {
+    const isdelete = confirm("are you sure to delete");
+    if (isdelete) {
+      this.http.delete("https://projectapi.gerasim.in/api/Complaint/DeletedepartmentBydepartmentId" + myid).subscribe((res: any) => {
+        if (res.result) {
+          alert('department Deleted succesfull');
+          this.getdepartment();
+        }
+        else {
+          alert(res.message);
+        }
+      })
+    }
+
   }
 }
